2d8a871fc0e66809f57c2500def46b21080a1fa4
context-coding <context.coding@gmail.com>
2021-11-11 22:24:00 -0600

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,52 @@
+<script>
+import FeedbackForm from "./components/FeedbackForm.svelte";
+
+import FeedbackList from "./components/FeedbackList.svelte";
+import FeedbackStats from "./components/FeedbackStats.svelte";
+
+// moved to the store!
+// this caused a lot of clean up in our code. Beatiful!
+// let feedback = [
+// 	{
+// 		id: 1,
+// 		rating: 10,
+// 		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+// 	},
+// 	{
+// 		id: 2,
+// 		rating: 9,
+// 		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+// 	},
+// 	{
+// 		id: 3,
+// 		rating: 8,
+// 		text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+// 	},
+// ];
+
+// moved to FeedbackStats component
+// $: count = feedback.length;
+// $: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length;
+
+
+// not needed anymore because of store
+// const deleteFeedback = (e) => {
+// 	const itemId = e.detail;
+// 	feedback = feedback.filter((item) => item.id !== itemId);
+// }
+
+// const addFeedback = (e) => {
+// 	console.log(e.detail);
+// 	feedback = [e.detail, ...feedback];
+// }
+
+</script>
+
+<main class="container">
+	<!-- <FeedbackForm on:add-feedback={addFeedback} /> -->
+	<!-- <FeedbackStats {count} {average} /> -->
+	<!-- <FeedbackList on:delete-feedback={deleteFeedback} /> -->
+	<FeedbackForm />
+	<FeedbackStats />
+	<FeedbackList />
+</main>
---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,30 @@
+<script>
+// import { onDestroy, onMount } from 'svelte';
+import { fade, scale, slide } from 'svelte/transition';
+import { FeedbackStore } from '../stores';
+import FeedbackItem from "./FeedbackItem.svelte";
+
+// let feedback = [];
+
+// const unsubscribe = FeedbackStore.subscribe((data) => feedback = data);
+
+// onMount(() => {
+//   console.log("component mounted.");
+// })
+
+// onDestroy(() => {
+//   console.log("component destroyed.");
+//   unsubscribe();
+// })
+
+</script>
+
+
+<!-- {#each feedback as fb (fb.id)} -->
+<!-- doing it with the store itself, will auto sub/unsubscribe for you so you don't need to do it mannually like above -->
+{#each $FeedbackStore as fb (fb.id)}
+<div in:fade out:slide="{{ duration: 1000 }}" >
+  <!-- <FeedbackItem item={fb} on:delete-feedback /> don't need this event forwarder anymore -->
+  <FeedbackItem item={fb} />
+</div>
+{/each}
